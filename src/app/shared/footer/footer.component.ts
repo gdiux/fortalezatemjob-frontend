fb6fa7c4baaa858c1b7f4297f450846d6211a1ea
Gilmer Contreras <adgdiux@gmail.com>
2022-08-24 18:11:34 -0500

    Cookies

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -9,16 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   public year = new Date().getFullYear();
+  public cookie: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    // COOKIES
+    let cokies = localStorage.getItem('cookies');
+
+    if (!cokies) {
+      this.cookie = false;
+    }else{
+      this.cookie = true;
+    }
     
     // PARALLAX FOOTER
     let footer:any = document.querySelector('footer');
     document.querySelector('.contenido')?.setAttribute('style', `margin-bottom:${footer.offsetHeight}px; position: relative; background: #fff;`);
     
 
+  }
+
+
+  /**=======================================================================
+  * ACCEPT COOKIES
+  ======================================================================== */
+  acceptCokies(){
+    localStorage.setItem('cookies', 'true');
+    this.cookie = true;
   }
 
 }
